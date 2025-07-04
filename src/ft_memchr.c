5b0f613d94bf0ea711bb12a863edde9076d108b7
Mahiro-3612 <m.h.s.k.australia@gmail.com>
2025-07-04 16:51:48 +0900

    feat: memchr

---
+++ src/ft_memchr.c
@@ -0,0 +1,23 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void *ft_memchr(const void *s, int c, size_t n)
+{
+    const unsigned char *s_ptr;
+    unsigned char c_char;
+
+    s_ptr = (const unsigned char *)s;
+    c_char = (unsigned char)c;
+    while (n > 0)
+    {
+        if (*s_ptr == c_char)
+        {
+            return s_ptr;
+        }
+        s_ptr++;
+        n--;
+    }
+    return NULL;
+}
