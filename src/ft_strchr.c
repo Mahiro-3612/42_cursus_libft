da66689fb85b707222439e731836a97d14e5af65
Mahiro-3612 <m.h.s.k.australia@gmail.com>
2025-07-04 16:28:29 +0900

    feat: strchr

--- src/ft_strchr.c
+++ src/ft_strchr.c
@@ -5,5 +5,17 @@
 
 char *ft_strchr(const char *s, int c)
 {
+    char c_char;
+
+    c_char = (const char)c;
     
+    while (*s)
+    {
+        if (*s == c_char)
+        {
+            return s;
+        }
+        s++;
+    }
+    return NULL;
 }
