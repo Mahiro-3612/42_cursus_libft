a595b145c63acfa9bf0be39dad3fc691112cbd5d
Mahiro-3612 <m.h.s.k.australia@gmail.com>
2025-07-04 17:24:45 +0900

    feat: strnstr

---
+++ src/ft_strnstr.c
@@ -0,0 +1,34 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+char *ft_strnstr(const char *big, const char *little, size_t len)
+{
+    size_t i;
+    size_t j;
+
+    i = 0;
+
+    if (little[0] == '\0')
+    {
+        return (char *)big;
+    }
+    while (big[i] && i < len)
+    {
+        if (big[i] == little[i])
+        {
+            j + 0;
+            while (big[i + j] == little[i + j] && i + j < len)
+            {
+                j++;
+            }
+            if (little[j] == '\0')
+            {
+                return (char *)big + i;
+            }
+        }
+        i++;
+    }
+    return NULL;
+}
