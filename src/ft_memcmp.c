5dcc7baa9dddb30c26101751b460eedc0ebd6a45
Mahiro-3612 <m.h.s.k.australia@gmail.com>
2025-07-04 17:01:14 +0900

    feat: memcmp

---
+++ src/ft_memcmp.c
@@ -0,0 +1,21 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+    const unsigned char *s1_ptr;
+    const unsigned char *s2_ptr;
+
+    s1_ptr = (const unsigned char *)s1;
+    s2_ptr = (const unsigned char *)s2;
+
+    while (n > 0 && *s1_ptr == *s2_ptr && *s1_ptr != '\0' && *s2_ptr != '\0')
+    {
+        s1_ptr++;
+        s2_ptr++;
+        n--;
+    }
+    return *s1_ptr - *s2_ptr;
+}
