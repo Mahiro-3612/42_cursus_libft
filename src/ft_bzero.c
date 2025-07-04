a9f31debe69675b8d279baa13b889e33f8b24dcb
Mahiro-3612 <m.h.s.k.australia@gmail.com>
2025-07-04 15:54:51 +0900

    feat: strlcpy

--- src/ft_bzero.c
+++ src/ft_bzero.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <bsd/string.h>
 
 void    *ft_bzero(void *s, size_t n)
 {
---
+++ src/ft_strlcpy.c
@@ -0,0 +1,43 @@
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+size_t ft_strlcpy(char *dest, const char *src, size_t size)
+{ 
+    size_t i;
+
+    i = 0;
+    while (i < size - 1 && src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    i = 0;
+    while (src[i] != '\0')
+    {
+        i++;
+    }
+
+   return i;
+}
+
+int main(void)
+{
+    char dest[10];
+    const char *src = "Hello, World!";
+    size_t copied;
+
+    copied = ft_strlcpy(dest, src, sizeof(dest));
+    printf("Copied string: '%s'\n", dest);
+    printf("Number of characters copied: %zu\n", copied);
+
+    // Test with a larger source string
+    const char *long_src = "This is a much longer string than the destination can hold.";
+    copied = ft_strlcpy(dest, long_src, sizeof(dest));
+    printf("Copied string: '%s'\n", dest);
+    printf("Number of characters copied: %zu\n", copied);
+
+    return 0;
+}
