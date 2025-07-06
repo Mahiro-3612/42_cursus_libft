a6af32a36b10420bfb46c0732c8c79e8cc26478c
Mahiro-3612 <m.h.s.k.australia@gmail.com>
2025-07-06 18:09:45 +0900

    fix: strchr

--- src/ft_strchr.c
+++ src/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: msakurai <msakurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 16:13:07 by msakurai          #+#    #+#             */
-/*   Updated: 2025/07/06 16:13:07 by msakurai         ###   ########.fr       */
+/*   Updated: 2025/07/06 18:09:17 by msakurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,29 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	char c_char;
+	char	c_char;
 
+	if (c == 0)
+	{
+		return ((char *)(s + ft_strlen(s)));
+	}
 	c_char = (const char)c;
-
 	while (*s)
 	{
 		if (*s == c_char)
 		{
-			return (char *)(s);
+			return ((char *)(s));
 		}
 		s++;
 	}
 	return (NULL);
 }
+
+// int	main(void)
+// {
+// 	assert(ft_strchr("Hello World", 'W') == strchr("Hello World", 'W'));
+// 	assert(ft_strchr("Hello World", 'z') == strchr("Hello World", 'z'));
+// 	assert(ft_strchr("", 'a') == strchr("", 'a'));
+// 	assert(ft_strchr("a", 0) == strchr("a", 0));
+// 	assert(ft_strchr("", 0) == strchr("", 0));
+// }
