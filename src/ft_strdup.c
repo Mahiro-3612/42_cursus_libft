08bfbe66710398777cf8202e5119aaf7130d357b
Mahiro-3612 <m.h.s.k.australia@gmail.com>
2025-07-06 12:19:35 +0900

    feat: strdup

---
+++ src/ft_strdup.c
@@ -0,0 +1,15 @@
+# include "../includes/libft.h"
+
+char *strdup(const char *s)
+{
+    char *copy;
+
+    copy = malloc(strlen(s) + 1);
+    if (copy == NULL)
+    {
+        return NULL;
+    }
+    ft_strlcpy(copy, s, strlen(s) + 1);
+    return copy;
+}
+
