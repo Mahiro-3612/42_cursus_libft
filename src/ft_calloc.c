cf33d52ca9a1aaf155d396a20d54f6b053999144
Mahiro-3612 <m.h.s.k.australia@gmail.com>
2025-07-06 12:13:43 +0900

    feat: calloc

---
+++ src/ft_calloc.c
@@ -0,0 +1,16 @@
+# include "../includes/libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+    unsigned char	*ptr;
+
+    if (nmemb == 0 || size == 0)
+        return NULL;
+
+    ptr = malloc(nmemb * size);
+    if (ptr == NULL)
+        return NULL;
+
+    ft_memset(ptr, 0, nmemb * size);
+    return ptr;
+}
