499d35cbd8c0c41ae3e9b2a64a191364316e2f46
Mahiro-3612 <m.h.s.k.australia@gmail.com>
2025-07-06 18:27:38 +0900

    fix: strrchr

--- src/ft_strrchr.c
+++ src/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: msakurai <msakurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 16:13:26 by msakurai          #+#    #+#             */
-/*   Updated: 2025/07/06 16:13:26 by msakurai         ###   ########.fr       */
+/*   Updated: 2025/07/06 18:26:04 by msakurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,13 @@
 
 char	*ft_strrchr(const char *s, int c)
 {
-	char c_char;
-	char *last_c;
+	char	c_char;
+	char	*last_c;
 
+	if (c == 0)
+	{
+		return ((char *)(s + ft_strlen(s)));
+	}
 	c_char = (const char)c;
 	last_c = NULL;
 	while (*s)
@@ -29,3 +33,12 @@ char	*ft_strrchr(const char *s, int c)
 	}
 	return (last_c);
 }
+
+// int	main(void)
+// {
+// 	assert(ft_strrchr("Wello World", 'W') == strrchr("Wello World", 'W'));
+// 	assert(ft_strrchr("Hello World", 'z') == strrchr("Hello World", 'z'));
+// 	assert(ft_strrchr("", 'a') == strrchr("", 'a'));
+// 	assert(ft_strrchr("a", 0) == strrchr("a", 0));
+// 	assert(ft_strrchr("", 0) == strrchr("", 0));
+// }
