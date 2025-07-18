abccded7741338a9a33206f719fb5c34765d6448
Mahiro Sakurai <m.h.s.k.australia@gmail.com>
2025-07-18 11:41:44 +0000

    fix: strlcat

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msakurai <msakurai@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 16:12:30 by msakurai          #+#    #+#             */
-/*   Updated: 2025/07/06 19:03:22 by msakurai         ###   ########.fr       */
+/*   Updated: 2025/07/18 11:39:37 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,11 @@ int	ft_atoi(const char	*str)
 	sign = 1;
 	while (*str == ' ' || *str == '\f' || *str == '\n' || *str == '\r'
 		|| *str == '\t' || *str == '\v')
-	{
 		str++;
-	}
 	if (*str == '+' || *str == '-')
 	{
 		if (*str == '-')
-		{
 			sign = -1;
-		}
 		str++;
 	}
 	num = 0;
@@ -40,9 +36,7 @@ int	ft_atoi(const char	*str)
 		str++;
 	}
 	if (num == (unsigned int)INT_MAX + 1 && sign == -1)
-	{
 		return (INT_MIN);
-	}
 	result = num * sign;
 	return (result);
 }
--- src/ft_strlcat.c
+++ src/ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msakurai <msakurai@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 16:13:12 by msakurai          #+#    #+#             */
-/*   Updated: 2025/07/06 17:55:01 by msakurai         ###   ########.fr       */
+/*   Updated: 2025/07/18 16:55:36 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,54 +15,36 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	i;
-	size_t	dst_size;
 	size_t	dst_len;
 	size_t	src_len;
 
-	dst_size = sizeof(dst);
+	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
-	if (dst_size <= size)
+	if (dst_len >= size)
 		return (size + src_len);
 	i = 0;
-	dst_len = ft_strlen(dst);
 	while (dst_len + i < size - 1 && src[i])
 	{
 		dst[dst_len + i] = src[i];
 		i++;
 	}
 	dst[dst_len + i] = '\0';
-	return (dst_len + i);
+	return (dst_len + src_len);
 }
+
+// #include <bsd/string.h>
 
 // int main(void)
 // {
-// 	char dst1[20] = "Hello, ";
-// 	const char *src1 = "World!";
-// 	size_t size1 = 20;
-// 	size_t result1 = ft_strlcat(dst1, src1, size1);
-// 	printf("Resulting string: %s\n", dst1);
-// 	printf("Total length: %zu\n", result1);
-
-// 	char dst2[20] = "Hello, ";
-// 	const char *src2 = "World!";
-// 	size_t size2 = 0;
-// 	size_t result2 = ft_strlcat(dst2, src2, size2);
-// 	printf("Resulting string with size: %s\n", dst2);
-// 	printf("Total length with size: %zu\n", result2);
-
-// 	char dst3[10] = "Hello, ";
-// 	const char *src3 = "World!";
-// 	size_t size3 = 15;
-// 	size_t result3 = ft_strlcat(dst3, src3, size3);
-// 	printf("Resulting string with size: %s\n", dst3);
-// 	printf("Total length with size: %zu\n", result3);
-
-// 	char dst4[14] = "a";
-// 	const char *src4 = "lorem ipsum dolor sit amet";
-// 	size_t size4 = 15;
-// 	size_t result4 = ft_strlcat(dst4, src4, size4);
-// 	printf("Resulting string with size: %s\n", dst4);
-// 	printf("Total length with size: %zu\n", result4);
+// 	char	dest1[30];
+// 	char	dest2[30];
+// 	const char *src = "AAAAAAAAA";
+// 	memset(dest1, 'B', 4);
+// 	memset(dest2, 'B', 4);
+// 	printf("%ld", ft_strlcat(dest1, src, 6));
+// 	printf("%ld\n", strlcat(dest2, src, 6));
+// 	assert(ft_strlcat(dest1, src, 6) == strlcat(dest2, src, 6));
+// 	assert(strcmp(dest1, dest2) == 0);
 
 // 	return (0);
 // }
