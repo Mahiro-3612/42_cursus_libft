9f0eb04261dff5501c2bae640f72b487252324b4
Mahiro Sakurai <m.h.s.k.australia@gmail.com>
2025-07-19 10:04:15 +0000

    fix: strjoin

--- src/ft_strjoin.c
+++ src/ft_strjoin.c
@@ -21,7 +21,16 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	dest = ft_calloc(s1_len + s2_len + 1, sizeof(char));
-	ft_strlcpy(dest, s1, s1_len);
-	ft_strlcpy(dest + s1_len, s2, s2_len);
+	if (!dest)
+		return (NULL);
+	ft_strlcpy(dest, s1, s1_len + 1);
+	ft_strlcpy(dest + s1_len, s2, s2_len + 1);
 	return (dest);
 }
+
+// int	main(void)
+// {
+// 	assert(strcmp(ft_strjoin("abc", "def"), "abcdef") == 0);
+// 	assert(strcmp(ft_strjoin("abc", ""), "abc") == 0);
+// 	return (0);
+// }
