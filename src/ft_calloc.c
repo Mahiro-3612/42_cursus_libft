ac18996d9ea8334f62c50500262fae8f49e2c918
Mahiro Sakurai <m.h.s.k.australia@gmail.com>
2025-07-21 14:11:59 +0000

    malloc(0)

--- src/ft_calloc.c
+++ src/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 16:12:37 by msakurai          #+#    #+#             */
-/*   Updated: 2025/07/21 08:55:04 by codespace        ###   ########.fr       */
+/*   Updated: 2025/07/21 14:10:38 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	unsigned char	*ptr;
 
-	if (nmemb == 0 || size == 0 || nmemb > SIZE_MAX / size)
+	if (nmemb == 0 || size == 0)
+		return (malloc(0));
+	if (nmemb > SIZE_MAX / size)
 		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
