0a829528f47a4e54b82fa4dbf13ec5bcb47c644c
Mahiro Sakurai <m.h.s.k.australia@gmail.com>
2025-07-21 06:40:48 +0000

    fix

--- bonus/ft_lstclear_bonus.c
+++ bonus/ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 05:35:10 by codespace         #+#    #+#             */
-/*   Updated: 2025/07/20 08:34:46 by codespace        ###   ########.fr       */
+/*   Updated: 2025/07/20 10:08:24 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,12 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*tmp;
 
-	if (!*lst || !*lst || !del)
+	if (!lst || !*lst || !del)
 		return ;
 	while (*lst)
 	{
 		tmp = (*lst)->content;
-		ft_lstdelone(tmp, del);
+		ft_lstdelone(*lst, del);
 		*lst = tmp;
 	}
 }
